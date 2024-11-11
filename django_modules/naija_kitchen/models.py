6a529a61a1b51f8229e1b403ce817a19e89bfd5b
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-11 17:21:09 +0100

    setting up API get

--- django_modules/naija_kitchen/models.py
+++ django_modules/naija_kitchen/models.py
@@ -5,3 +5,13 @@ from django.db import models
 
 class Person(models.Model):
     student_name = models.CharField(max_length=200)
+
+
+class Restaurant(models.Model):
+    restaurant_name = models.CharField(max_length=200)
+    restaurant_address = models.CharField(max_length=200)
+    restaurant_image = models.CharField(max_length=200)
+    restaurant_contact = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.restaurant_name
