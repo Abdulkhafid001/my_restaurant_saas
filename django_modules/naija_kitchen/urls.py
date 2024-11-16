11bd8ccbb5b8b6a4f12d038a1ab5831747cde0c4
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-16 01:20:59 +0100

    added menu and category routes to API

--- django_modules/naija_kitchen/urls.py
+++ django_modules/naija_kitchen/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from naija_kitchen.views import ListRestaurant, RestaurantDetail
+from naija_kitchen.views import *
 urlpatterns = [
-    path("", ListRestaurant.as_view(), name="Get Restaurant Names"),
-    path("restaurant/<int:pk>/", RestaurantDetail.as_view(), name="Get Restaurant Detail")
-] 
+    path("", ListRestaurant.as_view(), name="Restaurant Names"),
+    path("restaurant/<int:pk>/", RestaurantDetail.as_view(), name="Restaurant Detail"),
+    path("restaurant/<int:pk>/categories", ListMenuCategory.as_view(), name="Menu Category"),
+    path("restaurant/<int:pk>/categories/menuitem", ListMenuItem.as_view(), name="Menu Item")
+]
