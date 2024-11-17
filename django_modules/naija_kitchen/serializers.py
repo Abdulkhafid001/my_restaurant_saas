a36ba476bae2803216d6c5813d0088867846c9ef
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-17 17:32:37 +0100

    fixed serializers

--- django_modules/naija_kitchen/serializers.py
+++ django_modules/naija_kitchen/serializers.py
@@ -6,29 +6,6 @@ from rest_framework import serializers
 from naija_kitchen.models import Restaurant, MenuCategory
 
 
-class MenuCategorySerializer(serializers.HyperlinkedModelSerializer):
-    menu_items = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='menuitem-detail'
-    )
-
-    class Meta:
-        model = MenuCategory
-        fields = ['url', 'name', 'menu_items']
-
-
-# class MenuItemSerializer(serializers.HyperlinkedModelSeriali  zer):
-#     menu_categories = serializers.HyperlinkedRelatedField(
-#         many=True,
-#         read_only=True,
-#         view_name='menucategory-detail'
-#     )
-#     class Meta:
-#         model = MenuItem
-#         fields = ['url', 'name', 'menu_categories']
-
-
 class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     menu_categories = serializers.HyperlinkedRelatedField(
         many=True,
@@ -40,3 +17,18 @@ class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
         model = Restaurant
         fields = ['url', 'restaurant_name',
                   'menu_categories', 'restaurant_address']
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'name', 'description', 'price', 'is_available']
+
+
+class MenuCategorySerializer(serializers.ModelSerializer):
+    menu_items = MenuItemSerializer(
+        many=True, read_only=True)
+
+    class Meta:
+        model = MenuCategory
+        fields = ['id', 'name', 'description', 'menu_items']
