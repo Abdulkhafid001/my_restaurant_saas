ac075762f30a12a21f04d2ee8c9419f803299dfb
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-19 12:58:06 +0100

    adding restaurant search [Feature]

--- django_modules/naija_kitchen/app_views/restaurant_views.py
+++ django_modules/naija_kitchen/app_views/restaurant_views.py
@@ -1,7 +1,14 @@
-from django_modules.naija_kitchen.models import Restaurant
+import json
+from naija_kitchen.models import Restaurant
 from django.shortcuts import render
+from django.views.generic import ListView
 
 
-def get_all_restaurants(request):
-    all_restaurants = Restaurant.objects().all()
-    return render(request, 'restuarant_list.html', {'all_restaurants': all_restaurants})
+class RestaurantListView(ListView):
+    model = Restaurant
+    template_name = 'restaurant_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['qs_json'] = json.dumps(list(Restaurant.objects.values()))
+        return context
--- django_modules/naija_kitchen/urls.py
+++ django_modules/naija_kitchen/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from naija_kitchen.views import *
 from naija_kitchen import views
+from naija_kitchen.app_views.restaurant_views import RestaurantListView
 
 from rest_framework.routers import DefaultRouter
 
@@ -11,5 +12,6 @@ router.register(r'menucategories', views.MenuCategoryViewSet)
 urlpatterns = [
     path('', include(router.urls), name='api-home'),
     path('login', views.login, name='login'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path("myrestaurants/", RestaurantListView.as_view(), name="all restaurants")
 ]
