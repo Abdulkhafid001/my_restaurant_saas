a09c1447cbcd82812e6c6f67d9a799622207a4cf
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-24 04:43:10 +0100

    chore:fix add to cart

--- django_modules/cart/views.py
+++ django_modules/cart/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import json
-# Create your views here.
+from .models import *
+from naija_kitchen.models import MenuItem
 
 
 def update_cart(request):
@@ -9,12 +10,12 @@ def update_cart(request):
         data = json.loads(request.body.decode('utf-8'))
         product_id = data.get('productId')
         action = data.get('action')
+
         customer = request.user
-        print('order for customer: ', customer)
-        print('productId: ', product_id, 'action: ', action)
+        menu_item = MenuItem.objects.get(id=product_id)
+
+        print('customer: ', customer, 'menu item: ', menu_item)
+        # order, created = Order.objects.get_or_create(user=customer)
         # update cart logic here
         return JsonResponse("data sent successfully", safe=False, status=200)
     return JsonResponse({"error": "Invalid request"}, status=400)
-
-
-
