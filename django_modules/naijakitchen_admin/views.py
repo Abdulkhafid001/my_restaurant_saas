cf329f390fb29a30a63f42ae322cce8e68bc8543
barkeeya01 <barkeeyakhalifa123@gmail.com>
2024-12-08 14:38:49 +0100

    chore:adding admin homepage

--- django_modules/naijakitchen_admin/views.py
+++ django_modules/naijakitchen_admin/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-# Create your views here.
-
+from naija_kitchen.models import Restaurant
+from cart.models import Order
 def get_admin_home(request):
-    return HttpResponse("Start building admin here!")
+    all_restaurants = Restaurant.objects.all()
+    all_orders = Order.objects.all()
+    context = {'all_restaurants': all_restaurants, 'all_orders': all_orders}
+    return render(request, 'adminhome.html', context)
