3f8e45b5745f62b8808e2597049773b46ac254ed
barkeeya01 <barkeeyakhalifa123@gmail.com>
2024-12-17 13:30:57 +0100

    chore:adding adminhome

--- django_modules/naijakitchen_admin/views.py
+++ django_modules/naijakitchen_admin/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
 from naija_kitchen.models import Restaurant
 from cart.models import Order
+
+
 def get_admin_home(request):
     all_restaurants = Restaurant.objects.all()
     all_orders = Order.objects.all()
     context = {'all_restaurants': all_restaurants, 'all_orders': all_orders}
     return render(request, 'adminhome.html', context)
+ 
