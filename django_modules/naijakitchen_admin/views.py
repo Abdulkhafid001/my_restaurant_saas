60ef67454c4975382553990560b9e2b5c6451d0b
barkeeya01 <barkeeyakhalifa123@gmail.com>
2024-12-23 11:42:51 +0100

    chore:adding admin menumanagement

--- django_modules/naijakitchen_admin/views.py
+++ django_modules/naijakitchen_admin/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from naija_kitchen.models import Restaurant
+from naija_kitchen.models import Restaurant, MenuCategory, MenuItem
 from cart.models import Order
 
 
 def get_admin_home(request):
     all_restaurants = Restaurant.objects.all()
     all_orders = Order.objects.all()
-    context = {'all_restaurants': all_restaurants, 'all_orders': all_orders}
+    all_menu = MenuCategory.objects.all()
+    all_menu_items = MenuItem.objects.all()
+    context = {'all_restaurants': all_restaurants, 'all_orders': all_orders,
+               'all_menu': all_menu, 'all_menu_items': all_menu_items}
     return render(request, 'adminhome.html', context)
- 
